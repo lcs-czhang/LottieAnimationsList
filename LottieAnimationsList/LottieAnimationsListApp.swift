ddd1cf8eb0ec17b9401feb24caf7cb4f5ab2318b
Youzhou Zhang <czhang@lcs.on.ca>
2024-01-29 08:48:46 -0500

    Initial Commit

---
+++ LottieAnimationsList/LottieAnimationsListApp.swift
@@ -0,0 +1,17 @@
+//
+//  LottieAnimationsListApp.swift
+//  LottieAnimationsList
+//
+//  Created by Yuzhou Zhang on 2024-01-29.
+//
+
+import SwiftUI
+
+@main
+struct LottieAnimationsListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
